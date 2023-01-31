96ca1c9d5bf0810ee55dfb0a3fe7cebe5b946bf1
Mohankumarr07 <mohankumarr07@gmail.com>
2023-01-31 15:42:41 +0530

    upload success

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,42 @@
+package org.emp;
+
+public class Employee {
+
+	private void empId() {
+		System.out.println("empId=001");
+	}
+	private void empName() {
+		System.out.println("empName=Mohan");
+	}
+	private void empDob() {
+		System.out.println("empDob=12.06.1990");
+	}
+	private void empPhone() {
+		System.out.println("empPhone=9600430921");
+	}
+	private void empEmail() {
+		System.out.println("empEmail=Mohankumarr07@gmail.com");
+	}
+	private void empAddress() {
+		System.out.println("empAddress=4/20 salem");
+	}
+	public static void main(String args[]) {
+		
+		Employee E=new Employee();
+		
+		E.empId ();
+		E.empName();
+		E.empDob();
+		E.empPhone();
+		E.empEmail();
+		E.empAddress();
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
